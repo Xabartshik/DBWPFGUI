ff462b2d81d2450271ae916b28d87621c6207a14
Xabartshik <danoshlakov12@gmail.com>
2024-03-29 21:53:22 +0900

    Поправки в GUI

---
+++ DBWPFGUI/UIElements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace DBWPFGUI
+{
+
+    [TemplatePart(Name = "PART_CheckBox", Type = typeof(CheckBox))]
+    public class CheckableMenuItem : MenuItem
+    {
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register("IsChecked", typeof(bool), typeof(CheckableMenuItem), new PropertyMetadata(false));
+
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+    }
+}
